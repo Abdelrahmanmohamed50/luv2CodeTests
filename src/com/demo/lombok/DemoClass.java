0034c1dd8141825907b2a41d3145da2bb93101bb
Abdulrahman Helal <ahellal@ntgclarity.com>
2019-11-26 15:02:35 +0200

    first commit

---
+++ src/com/demo/lombok/DemoClass.java
@@ -0,0 +1,24 @@
+package com.demo.lombok;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//this @Data from lombok refer to setters and getters and hashing , equals  so no need to create them anymore in class
+//law 3aiz a7t getter bas or setter bas 
+//@Getter
+//@Setter
+//@EqualsAndHashCode
+//@NoArgsConstructor //bt create construcor fady
+
+//dol keda l kol wa7da 3la 7eda sawa2 mn fo2 5ales aw gnb kol variable
+
+public @Data class DemoClass {
+
+	private int age;
+	private String name;
+	private String tech;
+
+}
---
+++ src/com/demo/lombok/MainRunner.java
@@ -0,0 +1,13 @@
+package com.demo.lombok;
+
+public class MainRunner {
+
+	public static void main(String[] args) {
+
+		DemoClass demoClass = new DemoClass();
+		demoClass.setName("Abdulrahman");
+		System.out.println(demoClass);
+				
+	}
+
+}
